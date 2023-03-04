fd0c7e76957b9463a2ce85005b24fd255f7a2568
yasbhos <hossein.yasbolaghi@disroot.org>
2023-03-04 22:48:52 +0330

    Added header.php, footer.php templates

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once("config.php");
+
+include_once("templates/header.php");
+
+include_once("templates/footer.php");
---
+++ templates/header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?php
+        if (function_exists("get_title")) {
+            echo get_title();
+        } else {
+            echo APP_TITLE;
+        }
+        ?>
+    </title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="col-lg-12">
+    <nav class="py-2 bg-light border-bottom">
+        <div class="container d-flex flex-wrap">
+            <ul class="nav me-auto">
+                <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Dashboard</a></li>
+            </ul>
+            <ul class="nav">
+                <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Login</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Logout</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Sign up</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <main>
